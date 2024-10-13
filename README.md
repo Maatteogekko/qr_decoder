9387086925580d8ada5677df82b7a80da3d91568
Maatteogekko <matteogent9991@gmail.com>
2024-10-13 15:22:22 +0200

    provide the service on a server

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+# qr_decoder
+
 ## Setup
 
 In order to make this program work, `pdfium` must be installed on the system or its dynamic library must be present in the same folder as the executable.
 See [here](https://docs.rs/pdfium-render/0.8.25/pdfium_render/#binding-to-pdfium) for more info.
+
+## Use
+
+Look at <api.rest> for an example of how to invoke the service.
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,71 @@
-use std::{
-    collections::{HashMap, HashSet},
-    path::Path,
-};
-
-use clap::Parser;
-use image::{DynamicImage, EncodableLayout};
+use actix_multipart::form::{json::Json as MPJson, tempfile::TempFile, MultipartForm};
+use actix_web::{post, App, HttpResponse, HttpServer, Responder, Result};
+use image::{DynamicImage, EncodableLayout, ImageFormat};
 use pdfium_render::prelude::*;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use rxing::{BarcodeFormat, DecodeHintType, DecodeHintValue};
+use rxing::{BarcodeFormat, DecodeHintType, DecodeHintValue, DecodingHintDictionary};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::Read,
+    path::Path,
+    sync::{Arc, Mutex},
+};
 
-/// Simple CLI to extract QR codes from a PDF or image file
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Input file
-    #[arg(short, long)]
-    input: String,
-    /// Comma separated list of barcode formats to detect.
-    /// Supported values are:
-    /// - AZTEC
-    /// - CODABAR
-    /// - CODE_39
-    /// - CODE_93
-    /// - CODE_128
-    /// - DATA_MATRIX
-    /// - EAN_8
-    /// - EAN_13
-    /// - ITF
-    /// - MAXICODE
-    /// - PDF_417
-    /// - QR_CODE
-    /// - MICRO_QR_CODE
-    /// - RECTANGULAR_MICRO_QR_CODE
-    /// - RSS_14
-    /// - RSS_EXPANDED
-    /// - TELEPEN
-    /// - UPC_A
-    /// - UPC_E
-    /// - UPC_EAN_EXTENSION
-    /// - DXFilmEdge
-    #[arg(short, long, value_delimiter = ',')]
+#[derive(Debug, Deserialize)]
+struct Config {
     formats: Option<Vec<BarcodeFormat>>,
 }
 
-fn main() -> Result<(), String> {
-    let args = Args::parse();
+#[derive(Debug, MultipartForm)]
+struct UploadForm {
+    #[multipart(limit = "20MB")]
+    file: TempFile,
+    json: MPJson<Config>,
+}
 
-    // TODO JSON output
-    // TODO web server
-    // TODO docker
+#[derive(Debug, Serialize)]
+struct BarcodeData {
+    r#type: String,
+    data: String,
+}
 
-    let hints = create_hints(args.formats);
+#[derive(Debug, Serialize)]
+struct ErrorResponse {
+    message: String,
+}
 
-    get_images(&args.input)?.par_iter().for_each(|image| {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().service(scan_file))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[post("/scanner/scan")]
+async fn scan_file(MultipartForm(form): MultipartForm<UploadForm>) -> impl Responder {
+    let file_path = form.file.file.path();
+    let hints = create_hints(form.json.formats.clone());
+
+    match process_file(file_path, hints).await {
+        Ok(barcodes) => HttpResponse::Ok().json(barcodes),
+        Err(e) => HttpResponse::InternalServerError().json(ErrorResponse {
+            message: e.to_string(),
+        }),
+    }
+}
+
+/// Process the file and extract barcodes.
+async fn process_file(
+    path: &Path,
+    hints: Option<DecodingHintDictionary>,
+) -> Result<Vec<BarcodeData>, String> {
+    let images = get_images(&path).map_err(|e| e.to_string())?;
+    let barcode_list = Arc::new(Mutex::new(Vec::new()));
+
+    images.par_iter().for_each(|image| {
         let width = image.width();
         let height = image.height();
         let luma_image: Vec<u8> = image.clone().into_luma8().as_bytes().into();
@@ -64,17 +77,21 @@ fn main() -> Result<(), String> {
             None => rxing::helpers::detect_multiple_in_luma(luma_image, width, height),
         };
 
-        match results {
-            Ok(results) => {
-                for result in results {
-                    println!("{} -> {}", result.getBarcodeFormat(), result.getText());
-                }
+        if let Ok(results) = results {
+            for result in results {
+                let mut list = barcode_list.lock().expect("acquired Mutex");
+                list.push(BarcodeData {
+                    r#type: result.getBarcodeFormat().to_string(),
+                    data: result.getText().to_string(),
+                });
             }
-            Err(e) => eprintln!("Error decoding barcodes: {}", e),
         }
     });
 
-    Ok(())
+    Ok(Arc::into_inner(barcode_list)
+        .expect("valid Arc")
+        .into_inner()
+        .expect("valid Mutex"))
 }
 
 /// Creates barcode detection hints from the given formats.
@@ -91,15 +108,24 @@ fn create_hints(
 
 /// Gets images from the provided file path, handling different formats.
 fn get_images(path: &impl AsRef<Path>) -> Result<Vec<DynamicImage>, String> {
-    let kind = infer::get_from_path(path).map_err(|_| "Failed to read file".to_string())?;
-    let kind = kind.ok_or_else(|| "Unknown file type".to_string())?;
+    let kind = infer::get_from_path(path)
+        .map_err(|_| "Failed to read file".to_string())?
+        .ok_or_else(|| "Unknown file type".to_string())?;
+
+    let mut file = File::open(path).map_err(|e| format!("Failed to open file: {}", e))?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)
+        .map_err(|e| format!("Failed to read file: {}", e))?;
 
     match kind.mime_type() {
         "application/pdf" => {
             extract_images(path).map_err(|e| format!("Failed to extract images from PDF: {:?}", e))
         }
-        "image/jpeg" | "image/png" | "image/gif" | "image/webp" | "image/tiff" | "image/bmp" => {
-            image::open(path)
+        mime_type @ ("image/jpeg" | "image/png" | "image/gif" | "image/webp" | "image/tiff"
+        | "image/bmp") => {
+            let format = ImageFormat::from_mime_type(mime_type).expect("found mime_type");
+
+            image::load_from_memory_with_format(&buffer, format)
                 .map(|img| vec![img])
                 .map_err(|e| format!("Failed to read image: {}", e))
         }
