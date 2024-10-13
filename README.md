02ecc98920e759b4d4fd8972b91a661945b7cf00
Maatteogekko <matteogent9991@gmail.com>
2024-10-13 12:02:39 +0200

    handle images and allow to specify formats to scan for

---
+++ README.md
@@ -0,0 +1,4 @@
+## Setup
+
+In order to make this program work, `pdfium` must be installed on the system or its dynamic library must be present in the same folder as the executable.
+See [here](https://docs.rs/pdfium-render/0.8.25/pdfium_render/#binding-to-pdfium) for more info.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::BorrowMut,
     collections::{HashMap, HashSet},
     path::Path,
 };
@@ -15,26 +16,40 @@ struct Args {
     /// Input PDF file
     #[arg(short, long)]
     input: String,
+    /// List of barcode formats to detect
+    #[arg(short, long, value_delimiter = ',')]
+    formats: Option<Vec<BarcodeFormat>>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let images = extract_images(&args.input).expect("extracted images");
+    // TODO JSON output
+    // TODO web server
+    // TODO docker
 
-    for image in images {
+    let hints = args.formats.map(|formats| {
+        HashMap::from([(
+            DecodeHintType::POSSIBLE_FORMATS,
+            DecodeHintValue::PossibleFormats(HashSet::from_iter(formats.iter().cloned())),
+        )])
+    });
+
+    for image in get_images(&args.input) {
         let width = image.width();
         let height = image.height();
-        let luma_image = image.into_luma8().as_bytes().into();
+        let luma_image: Vec<u8> = image.into_luma8().as_bytes().into();
 
-        if let Ok(results) = rxing::helpers::detect_multiple_in_luma_with_hints(
-            luma_image,
-            width,
-            height,
-            &mut HashMap::from([(
-                DecodeHintType::POSSIBLE_FORMATS,
-                DecodeHintValue::PossibleFormats(HashSet::from([BarcodeFormat::QR_CODE])),
-            )]),
+        if let Ok(results) = hints.clone().map_or(
+            rxing::helpers::detect_multiple_in_luma(luma_image.clone(), width, height),
+            |mut hints| {
+                rxing::helpers::detect_multiple_in_luma_with_hints(
+                    luma_image,
+                    width,
+                    height,
+                    hints.borrow_mut(),
+                )
+            },
         ) {
             for result in results {
                 println!("{} -> {}", result.getBarcodeFormat(), result.getText())
@@ -43,18 +58,29 @@ fn main() {
     }
 }
 
+fn get_images(path: &impl AsRef<Path>) -> Vec<DynamicImage> {
+    let kind = infer::get_from_path(path)
+        .expect("file read successfully")
+        .expect("file type is known");
+
+    match kind.mime_type() {
+        "application/pdf" => extract_images(path).expect("extracted images"),
+        "image/jpeg" | "image/png" | "image/gif" | "image/webp" | "image/tiff" | "image/bmp" => {
+            vec![image::open(path).expect("file read successfully")]
+        }
+        filetype => panic!("Unexpected file type, {filetype}"),
+    }
+}
+
 fn extract_images(path: &impl AsRef<Path>) -> Result<Vec<DynamicImage>, PdfiumError> {
     let pdfium = Pdfium::default();
-
-    let document = pdfium.load_pdf_from_file(path, None)?;
-
     let render_config = PdfRenderConfig::new()
         .set_target_width(1000)
         .set_maximum_height(1000)
         .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
 
+    let document = pdfium.load_pdf_from_file(path, None)?;
     let mut images: Vec<DynamicImage> = Vec::new();
-
     for page in document.pages().iter() {
         images.push(page.render_with_config(&render_config)?.as_image());
     }
